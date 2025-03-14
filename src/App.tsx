17dc5d0c9245e9182aca785e5e3260388f9dd629
daolam1234 <lam15081999@gmail.com>
2025-03-14 21:58:41 +0700

    thêm db va cac trang quan ly san pham, danh muc

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,12 @@
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import HomePage from './pages/client/HomePage'
+import ProductList from './pages/admin/product/ProductList';
+import ProductAdd from './pages/admin/product/ProductAdd';
+import ProductEdit from './pages/admin/product/ProductEdit';
+import CategoryList from './pages/admin/category/CategoryList';
+import CategoryAdd from './pages/admin/category/CategoryAdd';
+import CategoryEdit from './pages/admin/category/CategoryEdit';
 
 
 function App() {
@@ -11,6 +17,13 @@ function App() {
   <>
  <Routes>
   <Route path='/homepage' element={< HomePage/>} />
+  <Route path='/admin/product' element={< ProductList/>} />
+  <Route path='/admin/product-add' element={< ProductAdd/>} />
+  <Route path='/admin/product-edit/:id' element={< ProductEdit/>} />
+  <Route path='/admin/category' element={< CategoryList/>} />
+  <Route path='/admin/category-add' element={< CategoryAdd/>} />
+  <Route path='/admin/category-edit/:id' element={< CategoryEdit/>} />
+  
 
  </Routes></>)
 }
